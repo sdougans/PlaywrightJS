43773c9937dd9cdccbe814daadf9461475adf80f
Stuart Dougans <stuart.dougans@gmail.com>
2023-12-19 23:03:59 +0000

    more checks added

--- playwright.config.js
+++ playwright.config.js
@@ -13,6 +13,9 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   timeout: 10 * 1000,
+  expect: {
+    timeout: 5 * 1000
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -41,15 +44,15 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/UiBasicsTest.spec.js
+++ tests/UiBasicsTest.spec.js
@@ -7,11 +7,20 @@ test('First Playwright test - fixtures', async ({browser})=> {
     console.log(await page.title());
     await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy")
 
+    await page.locator('#username').fill("rahulshetty");
+    await page.locator('#password').fill("learning");
+    await page.locator('#signInBtn').click();
+
+    await expect(page.locator('[style="display: block;"]')).toContainText('Incorrect username/password.');
+
+    await page.locator('#username').fill("rahulshettyacademy");
+    await page.locator('#signInBtn').click();
+    await expect(page).toHaveTitle('ProtoCommerce');
 });
 
-test('First Playwright test - basic', async ({page})=> {
-    await page.goto("https://google.com/");
-    console.log(await page.title());
-    await expect(page).toHaveTitle("Google")
+// test('First Playwright test - basic', async ({page})=> {
+//     await page.goto("https://google.com/");
+//     console.log(await page.title());
+//     await expect(page).toHaveTitle("Google")
 
-});
+// });
