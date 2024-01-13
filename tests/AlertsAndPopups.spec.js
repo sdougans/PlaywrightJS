0520a9df24108e92128360999beb3ec401422d59
Stuart Dougans <stuart.dougans@gmail.com>
2024-01-13 20:53:35 +0000

    more tests

--- tests/AlertsAndPopups.spec.js
+++ tests/AlertsAndPopups.spec.js
@@ -7,9 +7,14 @@ test.only('Playwright alerts and popups', async ({ page }) => {
     await expect(page.locator("#displayed-text")).toBeVisible();
     await page.locator("#hide-textbox").click();
     await expect(page.locator("#displayed-text")).toBeHidden();
-    await page.pause();
+    // await page.pause();
     page.on('dialog', dialog => dialog.accept());
     await page.locator("#confirmbtn").click();
     await page.locator("#mousehover").hover();
+
+    const framesPage = page.frameLocator("#courses-iframe");
+    await framesPage.locator("li a[href*='lifetime-access']:visible").click();
+    const textCheck = await framesPage.locator(".text h2").textContent();
+    console.log(textCheck.split(" ")[1]);
  
 });
